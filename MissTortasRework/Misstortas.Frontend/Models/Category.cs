2fb4e413752cf0001727e79d1c45a6485264ac7b
Elias Rojas <rojaseliasezequiel01@gmail.com>
2026-07-28 21:46:58 -0300

    dale que va

--- MissTortasRework/Misstortas.Frontend/Models/Category.cs
+++ MissTortasRework/Misstortas.Frontend/Models/Category.cs
@@ -5,6 +5,7 @@
         public long? ParentId { get; set; }
         public long ProductCategoryId { get; set; }
         public string Name { get; set; } = "";
+        public bool Enabled { get; set; }
         public Category[] Children { get; set; } = [];
         public bool IsFinal { get; set; }
     }
